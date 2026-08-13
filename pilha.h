6be3562ea92c70d595b406f3b2c62a7013466a82
André Brandão <andrebrandao@MacBook-Pro-de-Andre.local>
2026-08-13 16:22:09 -0300

    Criação da estrutura da pilha

--- pilha.h
+++ pilha.h
@@ -3,6 +3,17 @@
 
 #include <stdio.h>
 
+typedef int TipoItem;   // define o tipo de dados do elemento da pilha
 
+// Estrutura para lista encadeada
+struct no {
+    TipoItem info;
+    struct no* prox;
+} No;
+
+// Estrutura da pilha
+typedef struct {
+    No* topo;
+} Pilha;
 
 #endif /* pilha_h */
